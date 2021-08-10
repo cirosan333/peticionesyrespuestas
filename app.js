2511dd776519ecc71493b8458cd09d43f4c8dc3e
Ciro Santaigo Ionio <cirosantiago@live.com>
2021-08-10 14:26:14 -0300

    errores

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 require('dotenv').config();
+var pool = require('./models/db');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/admin/login');
 
 var app = express();
 
@@ -23,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/admin/login', loginRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
---
+++ routes/admin/login.js
@@ -0,0 +1,33 @@
+var express = require('express');
+const { getUserAndPassword } = require('../../models/usersModel');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.render('admin/login', {
+      layout:'admin/layout'
+  });
+});
+
+router.post('/', async (req, res, next) => {
+    try {
+        var usuario = req.body.usuario;
+        var password = req.body.password;
+
+        console.log(req.body);
+
+        var data = await usersModel.getUserAndPassword(user, password);
+
+        if(data != undefined){
+            res.redirect('/admin/novedades');
+        } else {
+            res.render('admin/login',{
+                layout: 'admin/layout',
+                error = true
+            })
+        }
+    }
+finally {}
+});
+
+module.exports = router;
