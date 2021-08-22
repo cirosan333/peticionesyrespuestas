761b13cc8411688a1d21ae76cc31481e137ce7a7
Ciro Santaigo Ionio <cirosantiago@live.com>
2021-08-22 14:12:50 -0300

    agregadas funciones en el admin

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -1,14 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var novedadesModel = require('./../../models/novedadesModel');
 
-
-/*obtiene la pagina de novedades */
-router.get('/', function(req, res, next) {
+/*obtiene las novedades */
+router.get('/', async function(req, res, next) {
+  var novedades = await novedadesModel.getNovedades();
   res.render('admin/novedades', {
       layout: 'admin/layout',
-      usuario: req.session.nombre
-      
+      novedades
   });
+});
+
+router.get('/eliminar/:id', async (req, res, next) => {
+  var id = req.params.id;
+  await novedadesModel.deleteNovedadesById(id);
+  res.redirect('/admin/novedades')
 });
 
 
